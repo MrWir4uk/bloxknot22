649c06ef213db36e06c2b9c85e3c5fd2af5b9d6d
MrWir4uk <152081630+MrWir4uk@users.noreply.github.com>
2024-03-03 17:31:07 +0200

    ацац

--- main.py
+++ main.py
@@ -14,6 +14,10 @@ class Widget(QMainWindow):
         self.ui.save_btn.clicked.connect(self.save_note)
         self.ui.vikno_zamit.clicked.connect(self.save_note)
         self.ui.delete_btn.clicked.connect(self.delete_note)
+        self.ui.add_teg.clicked.connect(self.add_tag_new)
+        self.ui.delete_teg.clicked.connect(self.del_teg)
+
+
 
 
 
@@ -21,11 +25,17 @@ class Widget(QMainWindow):
         self.name = self.ui.vikno_zamit.selectedItems()[0].text()
         self.ui.title_edit.setText(self.name)
         self.ui.text_edit.setText(self.notes[self.name]["текст"])
+        self.ui.listWidget_2.clear()
+        self.ui.listWidget_2.addItems(self.notes[self.name]["теги"])
 
     def save_note(self):
+        tags = []
+        for i in range(self.ui.listWidget_2.count()):
+            tags.append(self.ui.listWidget_2.item(i).text())
+
         self.notes[self.ui.title_edit.text()] = {
                 "текст": self.ui.text_edit.toPlainText(),
-                "теги": []
+                "теги": tags
 
             }
         with open("notes.json", "w", encoding="utf-8") as file:
@@ -36,7 +46,7 @@ class Widget(QMainWindow):
 
     def clear(self):
         self.ui.title_edit.clear()
-        self.ui.text_edit.clear()
+        self.ui.text_edit.clear()   
     def create_note(self):
         self.clear()
     
@@ -62,6 +72,23 @@ class Widget(QMainWindow):
             self.save_note()
         except:
             print("помилка видалення")
+
+    def add_tag_new(self):
+        tag_name = self.ui.tag_edit.text()
+        if tag_name!="":
+            if tag_name not in self.notes[self.name]["теги"]:
+                self.notes[self.name]["теги"].append(tag_name)
+                self.ui.listWidget_2.clear()
+                self.ui.listWidget_2.addItems(self.notes[self.name]["теги"])
+
+    def del_teg(self):
+        if self.ui.listWidget_2.selectedItems()[0]:
+            tag_name = self.ui.listWidget_2.selectedItems()[0].text()
+            if tag_name in self.notes[self.name]["теги"]:
+                self.notes[self.name]["теги"].remove(tag_name)
+                self.ui.listWidget_2.clear()
+                self.ui.listWidget_2.addItems(self.notes[self.name]["теги"])
+
 app = QApplication([])
 ex = Widget()
 ex.show()
